0736aba02529acee62eb50c2d9cb6ac5ac2a3d24
chinh.nguyen <chinh.nguyen@team.enouvo.com>
2026-06-07 10:15:07 +0700

    Initial Commit

---
+++ EzTrash/EzTrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  EzTrashApp.swift
+//  EzTrash
+//
+//  Created by Cinny on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct EzTrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
